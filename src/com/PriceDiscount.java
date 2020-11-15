9b93628bd924afd16b8048f726ff2d11dc8d9c86
nilaa1 <71128791+nilaa1@users.noreply.github.com>
2020-11-15 12:32:04 +0400

    Update PriceDiscount.java

--- src/com/PriceDiscount.java
+++ src/com/PriceDiscount.java
@@ -5,10 +5,14 @@ import java.util.Map;
 public class PriceDiscount implements Discount {
 	
 	@Override
-	public Double calculateDiscount(Double amt, Map<String,String> config) {
-		
-		return (Math.floor(amt/Double.valueOf(config.get("cutOffAmt")))*Double.valueOf(config.get("priceDiscount")));
-		
+	public Double calculateDiscount(Double amt, Map<String,String> config) {		
+		Double discountval ;
+		try{
+		discountval= (Math.floor(amt/Double.valueOf(config.get("cutOffAmt")))*Double.valueOf(config.get("priceDiscount")));
+		}catch(NumberFormatException e){
+			e.printStackTrace();
+		}
+		return discountval;
 	}
 
 }
